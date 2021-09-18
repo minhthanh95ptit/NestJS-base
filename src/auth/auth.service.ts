dfaeace926e297c672d2930c292fff502af8faee
Pham Minh Thanh <48878946+minhthanh95ptit@users.noreply.github.com>
2021-09-19 00:11:15 +0700

    Revert "Revert "Feat/login qr code""

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,12 @@ import { Request } from 'express';
 import { UsersService } from 'src/modules/users/users.service';
 import { MailService } from 'src/mail/mail.service';
 import AuthCreadentialsDto from './dto/auth-credentials.dto';
+import VerifyPassCodeDto from './dto/verify-pass-code.dto';
 import JwtPayload from './payloads/jwtPayload';
 import { AuthMessage } from './auth.constants';
 import TokenResponseDto from './dto/token-response.dto';
-
+import { genCode } from '../helpers/index'
+import * as QRCode from 'qrcode'
 @Injectable()
 export class AuthService {
   constructor(
@@ -35,21 +37,44 @@ export class AuthService {
    */
   async signIn(
     authCredentialsDto: AuthCreadentialsDto,
-  ): Promise<TokenResponseDto> {
+  ): Promise<boolean> {
     const { email, password } = authCredentialsDto;
     const user = await this.usersService.getByEmail(email);
 
     // If user with email exist and the password is valid.
     if (user && (await user.validatePassword(password))) {
+      const passCode = genCode(6, 'numeric');
+      console.log(passCode);
+      await this.usersService.update(user.id, { passCode });
+      await this.mailService.sendUserConfirmation(user, passCode);
+      return true;
+    }
+    // Else return an error.
+    throw new BadRequestException(AuthMessage.INVALID_CREDENTIALS);
+  }
+
+  async verifyPassCode(
+    verifyPassCodeDto: VerifyPassCodeDto,
+  ): Promise<TokenResponseDto> {
+    const { email, passCode } = verifyPassCodeDto;
+    const user = await this.usersService.getByEmail(email);
+
+    // If user with email exist and the password is valid.
+    if (user && user.passCode === passCode) {
       const payload: JwtPayload = { email };
       const jwtAccessToken = await this.jwtService.signAsync(payload);
-
-      // send confirmation mail
-      await this.mailService.sendUserConfirmation(user, jwtAccessToken);
       return { jwtAccessToken };
     }
     // Else return an error.
     throw new BadRequestException(AuthMessage.INVALID_CREDENTIALS);
+  }
+  
+  async generateQR(text: any): Promise<any>{
+    try {
+      console.log(await QRCode.toDataURL(text))
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   async externalSignIn(req: Request): Promise<Express.User> {
