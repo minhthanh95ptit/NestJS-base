eb7604216e30d282ff0c7cd7009a4669be93c129
Pham Minh Thanh <minhthanh95ptit@gmail.com>
2021-09-20 14:24:49 +0700

    #6 - mail-chimp-set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './configs/database/typeorm.config';
 import { UsersModule } from './modules/users/users.module';
+import { MailchimpModule } from '@ntegral/nestjs-mailchimp';
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, UsersModule,MailchimpModule.forRoot({
+    apikey: 'a542b8a9e402ee79cc872b5f2fa95c6e-us5'
+  }),],
   controllers: [],
   providers: [],
 })
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,23 +1,44 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { User } from '../modules/users/user.entity';
+// eslint-disable-next-line
+const mailchimp = require('@mailchimp/mailchimp_transactional')('a542b8a9e402ee79cc872b5f2fa95c6e-us5');
 
+
+const message = {
+  from_email: 'thanhpm@vmodev.com',
+  subject: 'Hello world',
+  text: 'Welcome to Mailchimp Transactional!',
+  to: [
+    {
+      email: 'minhthanh95ptit@gmail.com',
+      type: 'to',
+    },
+  ],
+};
 @Injectable()
 export class MailService {
   constructor(private mailerService: MailerService) {}
 
-  async sendUserConfirmation(user: User, otpCode: string) {
-    // const url = `example.com/auth/confirm?token=${token}`;
+  // async sendUserConfirmation(user: User, otpCode: string) {
+  //   // const url = `example.com/auth/confirm?token=${token}`;
 
-    await this.mailerService.sendMail({
-      to: user.email,
-      // from: '"Support Team" <support@example.com>', // override default from
-      subject: 'Welcome to Nice App! Confirm your Email',
-      template: './confirmation', // `.hbs` extension is appended automatically
-      context: { // ✏️ filling curly brackets with content
-        name: user.email,
-        otpCode,
-      },
+  //   await this.mailerService.sendMail({
+  //     to: user.email,
+  //     // from: '"Support Team" <support@example.com>', // override default from
+  //     subject: 'Welcome to Nice App! Confirm your Email',
+  //     template: './confirmation', // `.hbs` extension is appended automatically
+  //     context: { // ✏️ filling curly brackets with content
+  //       name: user.email,
+  //       otpCode,
+  //     },
+  //   });
+  // }
+
+  async sendUserConfirmation(user: User, otpCode: string) {
+    const response = await mailchimp.messages.send({
+      message,
     });
+    return response
   }
 }
